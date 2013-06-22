97d46230916fd99abb15c1a97614675f8f166f0c
Jesus Alliman <contact.freeenterpriserp@gmail.com>
2013-06-21 22:51:13 -0500

    Sorta Fixed Bomb

--- skeleton/entities/entities/ent_bomb/shared.lua
+++ skeleton/entities/entities/ent_bomb/shared.lua
@@ -16,9 +16,15 @@ AddCSLuaFile( "shared.lua" )
    Name: Initialize
 ---------------------------------------------------------*/
 function ENT:Initialize()
+	self.Entity:SetModel("models/Items/ammocrate_smg1.mdl")
+	self.Entity:PhysicsInit(SOLID_VPHYSICS)
+	self.Entity:SetMoveType(MOVETYPE_VPHYSICS)
+	self.Entity:SetSolid(SOLID_VPHYSICS)
+	self.Entity:DrawShadow(false)
+end
 
+function ENT:OnTakeDamage()
 	Msg( "Bomb Initialized" )
 	ents.Create("env_explosion")
 	Entity:Remove()
-
 end
